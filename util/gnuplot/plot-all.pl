ecf99bef1ac254a7de754543f7f4fe4451535f1a
Guido De Rosa <job@guidoderosa.net>
2009-04-21 21:03:08 +0200

    plot-all.pl can be safely launched from other directories

--- util/gnuplot/plot-all.pl
+++ util/gnuplot/plot-all.pl
@@ -1,16 +1,20 @@
 #!/usr/bin/perl
 
-$datadir="../../data";
+use File::Basename;
+
+$datadir = dirname($0)."/../../data";
 
 opendir DATADIR,$datadir or die "Couldn't open $datadir";
 
 open GNUPLOT,"|gnuplot";
 
+print GNUPLOT "cwd=\"".dirname($0)."\"\n";
+
 while($f=readdir DATADIR) {
     if ($f =~ m/\.dat$/) {
         $f =~ s/(.*)\.dat$/$1/; 
         print GNUPLOT "name=\"$f\"\n";
-        print GNUPLOT "load \"plot.gpi\"\n";
+        print GNUPLOT "load \"".dirname($0)."/plot.gpi\"\n";
     }
 }
 
